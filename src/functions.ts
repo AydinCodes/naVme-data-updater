6107f4b7dd25af57a879852a184bd6e68e3d97d7
Aydin Elhan <aydinmate@protonmail.com>
2023-08-21 23:05:30 +1000

    added prisma and src files

---
+++ src/functions.ts
@@ -0,0 +1,67 @@
+// createData.ts
+import { PrismaClient } from "@prisma/client";
+import readline from "readline";
+
+const prisma = new PrismaClient();
+
+export async function addUser(username: string, url: string) {
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        username,
+        url,
+      },
+    });
+
+    console.log("User: ", username, " created.");
+
+    return user;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getUserById(id: string) {
+  return await prisma.user.findFirst({
+    where: {
+      id: id,
+    },
+  });
+}
+
+
+export async function getUserByUsername(username: string) {
+  return await prisma.user.findFirst({
+    where: {
+      username: username,
+    },
+  });
+}
+
+export async function deleteAllUsers() {
+  await prisma.user.deleteMany();
+
+  console.log("All users deleted.");
+}
+
+export async function findAllUsers() {
+ const users =  await prisma.user.findMany();
+console.log("All users: ", users)
+return users
+}
+
+
+export const askPassword = () => {
+  return new Promise<string>((resolve) => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    rl.question('Enter the password: ', (password) => {
+      rl.close();
+      resolve(password);
+    });
+  });
+}
---
+++ src/tempRun.ts
@@ -0,0 +1,14 @@
+import { addUser, deleteAllUsers, findAllUsers, getUserByUsername } from "./functions";
+
+
+
+(async () => {
+    addUser("aydinmate", 'https://en.wikipedia.org/wiki/Main_Page');
+//   const user = await getUserByUsername("aydinmate");
+// await deleteAllUsers()
+//   console.log(user);
+
+// await findAllUsers()
+})();
+
+
